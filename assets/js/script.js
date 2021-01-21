258a4a5450457a82bfcc700113724233fa802ac9
G. Vishakh <60139520+vishakhg98@users.noreply.github.com>
2021-01-21 20:42:44 +0530

    Styling and initial working scripts.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-// const EmpName = document.querySelector("#name").innerHTML;
-// console.log(EmpName);
-// const sal = document.querySelector("#salary").value;
-// console.log(sal);
-// const yr = document.querySelector("#years").value;
-// console.log(yr);
-
 function calculate() {
-  var empName = salaryform.name.value;
-  var empSalary = parseFloat(salaryform.salary.value);
-  var years = parseInt(salaryform.years.value);
-  var months = parseInt(salaryform.months.value);
-  console.log(empName, empSalary, years, months);
+  var empName = form.name.value;
+  var empSalary = parseFloat(form.salary.value);
+  var years = parseInt(form.years.value);
+  var months = parseInt(form.months.value);
+
+  totalSalary = (
+    empSalary * (years * 12) +
+    empSalary * months
+  ).toLocaleString();
+  var results = "Total Salary :  " + totalSalary.toLocaleString();
+  console.log(results);
+  // &#8377; <- Rupees unicode
+  document.querySelector(".form__results").innerHTML =
+    "Total Salary &emsp;: &emsp; &#8377;" + totalSalary;
 }

03bd659cd470f87b50a3ac99afd9a70f4a6d13e9
G. Vishakh <60139520+vishakhg98@users.noreply.github.com>
2021-01-22 10:43:50 +0530

    Switched to onsubmit from onclick to call the function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
 function calculate() {
-  // var empName = form.name.value;
+  var empName = form.name.value;
   var empSalary = parseFloat(form.salary.value);
   var months = parseInt(form.months.value);
 
-  if (isNaN(empSalary) || isNaN(months)) {
-    totalSalary = "";
-  } else {
-    totalSalary = empSalary * months;
-    // To add commas to salary
-    totalSalary = totalSalary.toLocaleString();
-  }
+  totalSalary = empSalary * months;
+  // To add commas to salary
+  totalSalary = totalSalary.toLocaleString();
   // &#8377; <- Rupees unicode
-  document.querySelector(".form__results").innerHTML =
-    "Total Salary drawn &emsp;: &emsp; &#8377;" + totalSalary;
+  document.querySelector(
+    ".form__results"
+  ).innerHTML = `${empName}'s Total Salary &emsp;: &emsp; &#8377;  ${totalSalary}`;
 }
